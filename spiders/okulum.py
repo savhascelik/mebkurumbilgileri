8944179c70adc3ac44298139f0298ac51ad054e9
Savaş HASÇELİK <7150420+savhascelik@users.noreply.github.com>
2022-03-18 16:12:04 +0300

    first record

---
+++ spiders/okulum.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import urllib.parse
+
+
+class OkulumSpider(scrapy.Spider):
+    name = 'okulum'
+    allowed_domains = ['http://www.meb.gov.tr/baglantilar/okullar/?ILKODU=1']
+    start_urls = ['http://www.meb.gov.tr/baglantilar/okullar/?ILKODU=1']
+    okulbilgileri=[]
+    for x in range(2,82):
+        text="http://www.meb.gov.tr/baglantilar/okullar/?ILKODU="+str(x)
+        start_urls.append(text)
+        
+
+    def parse(self, response):
+        okuladi = response.css(".table tr td:first-child a::text").extract()
+        okulsite = response.css(".table tr td:first-child a::attr(href)").extract()
+        okulhakkinda=response.css(".table tr td:nth-child(2) a::attr(href)").extract()
+        okulharita=response.css(".table tr td:nth-child(3) a::attr(data-src)").extract()
+     
+        for item in zip(okuladi,okulsite,okulhakkinda,okulharita):
+            okulbilgileri={
+                "adi":item[0],
+                "site":item[1],
+                "hakkinda":item[2],
+               "okulharita":item[3],
+            }
+            
+
+            yield okulbilgileri
+  
